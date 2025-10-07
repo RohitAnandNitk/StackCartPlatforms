a044a044d4a499ffc42c9c9f2d3c1f30683900a3
RohitAnandNitk <sharmarohit64748@gmail.com>
2025-10-07 23:53:23 +0530

    certificate app link updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -71,14 +71,14 @@ function Home() {
     {
       icon: Code,
       title: "Code to Cart",
-      link: "http://stackcart.space",
+      link: "http://stackcart.tech",
       className: "platform",
       description: "Explore our Tech Hub",
     },
     {
       icon: Award,
       title: "Certificate Validation",
-      link: "https://stackcart-certify.vercel.app/",
+      link: "https://www.stackcart.space/",
       className: "certificate",
       description: "Verify work credentials",
     },
